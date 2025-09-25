181594bbb69a75f5aaa8257add728722ee75b985
Joseia Jose Baby(UST <u38043@ust.com>
2025-09-25 20:09:56 +0530

    change

--- src/main/java/com/example/demo/entity/Admin.java
+++ src/main/java/com/example/demo/entity/Admin.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "control")
+@Table(name = "approver")
 public class Admin {
     @Id
     private String adminId;
